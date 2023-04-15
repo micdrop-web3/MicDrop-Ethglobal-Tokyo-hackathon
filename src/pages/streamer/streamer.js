9cd47b0228d4ce100641d562773e52e3a27373a0
atwanskas <61622684+atwanskas@users.noreply.github.com>
2023-04-15 11:36:49 +0530

    Create streamer.js

---
+++ src/pages/streamer/streamer.js
@@ -0,0 +1,30 @@
+export const streambtn = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+export const streamer = {
+  backgroundColor: "lightgrey", 
+  paddingBottom: "200px"
+}
+
+export const videobox = {
+  marginTop: '50px',
+  height: '600px',
+  width: '850px',
+  border: '10px solid black',
+};
+
+export const button = {
+  background: '#30989e',
+  margin: '30px',
+  padding: '20px',
+  border: 'none',
+};
+
+export const btn1 = {
+  background: '#30989e',
+  margin: '30px',
+  padding: '0 46px',
+  border: 'none',
+};
